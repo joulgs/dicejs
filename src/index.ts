673b32d9b2ce985b1e099b09fb3d6d8c90074e57
joulgs <joul.gs@gmail.com>
2023-04-25 22:19:37 -0300

    multi language

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,40 @@
+import { sprintf } from 'sprintf-js';
+
 export class Dice {
-    // constructor(language: string) {
-    //     this.roll(roll);
-    // }
+    private language: string = 'en';
+
+    private messages: any = {
+        en: {
+            rolling: 'Rolling %sd%s...\n',
+            diceRoll: "Dice %d rolled %d .",
+            roll: "Roll %s rolled %d ."
+        },
+        'pt-br': {
+            rolling: 'Lançando %sd%s...\n',
+            diceRoll: "Dado %d rolou %d .",
+            roll: "Rolagem %s rolou %d ."
+        }
+    }
+    
+    constructor(language: string = 'en') {
+        this.language = language;
+    }
 
     roll(roll = 'd6') {
 
         const [dices, sides] :any = roll.split('d').map((v, i) => v || (i === 0 ? 1 : 6));
         
-        console.log('Rolling ' + dices + 'd' + sides + '...')
+        console.log(sprintf(this.messages[this.language]?.rolling, dices, sides));
         
         let total = 0;
-        
         for (let i = 0; i < dices; i++) {
             const roll = Math.floor(Math.random() * sides) + 1;
-            console.log('Dice ' + (i + 1) + ' rolled ' + roll + '!');
+            console.log(sprintf(this.messages[this.language]?.diceRoll, (i + 1), roll));
+
             total += roll;
         }
-
-        console.log(dices + 'D' + sides + ' rolled ' + total + '!');
-        // return total;
+        
+        console.log(sprintf(this.messages[this.language]?.roll, roll, total));
     }
 }
   
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,10 @@
 import { Dice } from './index';
 
-const player1 = new Dice();
-player1.roll('3d6');
+// const playerGringo = new Dice();
+// playerGringo.roll('3d6');
+
+const player2 = new Dice('pt-br');
+player2.roll('2d20');
 
 //player1.toString();
 //player1.roll();
