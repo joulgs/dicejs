435984016f3fb5735237fc667c5216ca3dc98c00
joulgs <joul.gs@gmail.com>
2023-04-25 21:43:12 -0300

    using class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-export { default as roll } from './roll';
+export class Dice {
+    // constructor(language: string) {
+    //     this.roll(roll);
+    // }
+
+    roll(roll = 'd6') {
+
+        const [dices, sides] :any = roll.split('d').map((v, i) => v || (i === 0 ? 1 : 6));
+        
+        console.log('Rolling ' + dices + 'd' + sides + '...')
+        
+        let total = 0;
+        
+        for (let i = 0; i < dices; i++) {
+            const roll = Math.floor(Math.random() * sides) + 1;
+            console.log('Dice ' + (i + 1) + ' rolled ' + roll + '!');
+            total += roll;
+        }
+
+        console.log(dices + 'D' + sides + ' rolled ' + total + '!');
+        // return total;
+    }
+}
+  
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,7 @@
-import { roll } from './index';
+import { Dice } from './index';
 
-console.log(roll('2d6'));
+const player1 = new Dice();
+player1.roll('3d6');
+
+//player1.toString();
+//player1.roll();
